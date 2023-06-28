b9602b59a351dff7f48b97fb1975b9fdec5235c1
unknown <matheus.duartesilva42@gmail.com>
2023-06-28 18:45:54 -0300

    implementação do loading

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from 'react'
 import './Products.css'
 import fetchProducts from '../../api/fetchProducts'
 import ProductCard from '../ProductCard/ProductCard'
+import Loading from '../Loading/Loading'
 
 const Products = () => {
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         fetchProducts('iphone').then((response) => {
             setProducts(response)
+            setLoading(false)
         })
     }, [])
 
     return (
-        <section className='products container'>
-            {products.map((product) => <ProductCard key={product.id} data={product} />)}
-        </section>
+        (loading && <Loading />) || (
+            <section className='products container'>
+                {products.map((product) => <ProductCard key={product.id} data={product} />)}
+            </section>
+        )
+
     )
 }
 
