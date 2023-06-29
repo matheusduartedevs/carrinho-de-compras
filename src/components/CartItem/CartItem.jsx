ac1b55ea688140f57e1ccfa220f96722faa18a17
unknown <matheus.duartesilva42@gmail.com>
2023-06-29 09:23:28 -0300

    implementação de remover itens do carrinho

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BsCartDashFill } from 'react-icons/bs'
 import formatCurrency from '../../utils/formatCurrency'
 import propTypes from 'prop-types'
+import AppContext from '../context/AppContext'
 import './CartItem.css'
 
 const CartItem = ({ data }) => {
-  const { thumbnail, title, price } = data
+  const { cartItems, setCartItems } = useContext(AppContext)
+  const { id ,thumbnail, title, price } = data
+
+  const handleRemoveItem = () => {
+    const updatedItems = cartItems.filter((item) => item.id != id)
+    setCartItems(updatedItems)
+  }
 
   return (
     <section className="cart-item">
@@ -18,7 +25,11 @@ const CartItem = ({ data }) => {
             <h3 className="cart-item-title">{title}</h3>
             <h3 className="cart-item-price">{formatCurrency(price, 'BRL')}</h3>
 
-            <button type="button" className="button-remove-item">
+            <button 
+              type="button" 
+              className="button-remove-item"
+              onClick={ handleRemoveItem }
+            >
                 <BsCartDashFill />
             </button>
         </div>
